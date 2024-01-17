b2b24ab8b06597b59aee11ae4fe902cf3e616f89
Daniil Vilnyanskiy <103027487+DaniilVilnyanskiy@users.noreply.github.com>
2024-01-17 23:48:14 +0400

    feat: create main layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('content')
+    <x-ui.button text="button" />
+    <p>This is my body content.</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+        col
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+        col
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+        col
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+        col
+    </div>
+
+@stop
+
